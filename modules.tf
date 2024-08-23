e847ec82ee13b504c3a36e92b12294ccd42e450f
mikamakusa <michael.dangleterre@gmail.com>
2024-08-23 11:32:37 +0200

    update

--- modules.tf
+++ modules.tf
@@ -1,10 +1,10 @@
 module "identity" {
-  source      = "modules/terraform-oci-identity"
+  source      = "./modules/terraform-oci-identity"
   compartment = var.compartment
 }
 
 module "core" {
-  source      = "modules/terraform-oci-core"
+  source      = "./modules/terraform-oci-core"
   compartment = var.compartment
   subnet      = var.subnet
 }
